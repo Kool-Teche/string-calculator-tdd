c67b4fe029fb98244652db1e6d492798f7bf2a14
kool-teche <koolteche@gmail.com>
2025-02-15 14:34:32 +0530

    Implemented single number and two numbers test

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -4,3 +4,13 @@ test("returns 0 for an empty string", () => {
   const calc = new StringCalculator();
   expect(calc.add("")).toBe(0);
 });
+
+test("returns the number itself for a single number", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("1")).toBe(1);
+});
+
+test("returns sum of two numbers", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("1,2")).toBe(3);
+});
